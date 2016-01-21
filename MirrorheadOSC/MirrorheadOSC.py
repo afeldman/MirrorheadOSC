b69f141d29c36ed4ec9429aa98b8c84443fb92fd
Anton Feldmann <anton.feldmann@daimler.com>
2016-01-21 12:00:50 +0100

    test mit udp und standard test lib

--- MirrorheadOSC/MirrorheadOSC.py
+++ MirrorheadOSC/MirrorheadOSC.py
@@ -8,14 +8,59 @@ if __name__ == "__main__":
     ip = "192.168.0.200"
     port = 7475
 
-    client = udp_client.UDPClient(ip, port)
+    try:
+        client = udp_client.UDPClient(ip, port)
+    except:
+        print("cannot open client")
 
-    msg = osc_message_builder.OscMessageBuilder(address = "/mdc_layer1_presert1")
+    msg = osc_message_builder.OscMessageBuilder(address = "/mdc_layer1_preset1")
+    msg = msg.build()
+    client.send(msg)
+
+    time.sleep(1)
+
+    msg = osc_message_builder.OscMessageBuilder(address = "/mdc_layer1_preset19")
     msg = msg.build()
     client.send(msg)
    
-    time.sleep(5)
+    time.sleep(1)
 
-    msg = osc_message_builder.OscMessageBuilder(address = "/mdc_layer1_presert2")
+    msg = osc_message_builder.OscMessageBuilder(address = "/mdc_layer1_preset13")
+    msg = msg.build()
+    client.send(msg)
+
+    time.sleep(1)
+
+    msg = osc_message_builder.OscMessageBuilder(address = "/mdc_layer1_preset14")
+    msg = msg.build()
+    client.send(msg)
+
+    time.sleep(1)
+
+    msg = osc_message_builder.OscMessageBuilder(address = "/mdc_layer1_preset15")
+    msg = msg.build()
+    client.send(msg)
+
+    time.sleep(1)
+
+    msg = osc_message_builder.OscMessageBuilder(address = "/mdc_layer1_preset16")
+    msg = msg.build()
+    client.send(msg)
+
+    time.sleep(1)
+
+    msg = osc_message_builder.OscMessageBuilder(address = "/mdc_layer1_preset17")
+    msg = msg.build()
+    client.send(msg)
+
+    time.sleep(10)
+
+    msg = osc_message_builder.OscMessageBuilder(address = "/mdc_layer1_preset18")
+    msg = msg.build()
+    client.send(msg)
+
+    time.sleep(1)
+
+    msg = osc_message_builder.OscMessageBuilder(address = "/mdc_layer1_preset20")
     msg = msg.build()
     client.send(msg)
