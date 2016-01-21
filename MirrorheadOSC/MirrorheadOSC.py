9e0449ec7d83064ff7f3877cfc20cc66d1b2addc
emea\feldmaa <anton.feldmann@daimler.com>
2016-01-21 13:31:56 +0100

    test daimlerset

--- MirrorheadOSC/MirrorheadOSC.py
+++ MirrorheadOSC/MirrorheadOSC.py
@@ -13,36 +13,6 @@ if __name__ == "__main__":
     except:
         print("cannot open client")
 
-    msg = osc_message_builder.OscMessageBuilder(address = "/mdc_layer1_preset1")
-    msg = msg.build()
-    client.send(msg)
-
-    time.sleep(1)
-
-    msg = osc_message_builder.OscMessageBuilder(address = "/mdc_layer1_preset2")
-    msg = msg.build()
-    client.send(msg)
-   
-    time.sleep(1)
-
-    msg = osc_message_builder.OscMessageBuilder(address = "/mdc_layer1_preset3")
-    msg = msg.build()
-    client.send(msg)
-
-    time.sleep(1)
-
-    msg = osc_message_builder.OscMessageBuilder(address = "/mdc_layer1_preset4")
-    msg = msg.build()
-    client.send(msg)
-
-    time.sleep(1)
-
-    msg = osc_message_builder.OscMessageBuilder(address = "/mdc_layer1_preset5")
-    msg = msg.build()
-    client.send(msg)
-
-    time.sleep(1)
-
     msg = osc_message_builder.OscMessageBuilder(address = "/mdc_layer1_media4")
     msg = msg.build()
     client.send(msg)
