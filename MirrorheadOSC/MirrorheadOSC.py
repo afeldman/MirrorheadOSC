13c68c22e883487d13a32c366430427618f6a42e
Anton Feldmann <anton.feldmann@daimler.com>
2016-01-21 12:04:35 +0100

    test daimler

--- MirrorheadOSC/MirrorheadOSC.py
+++ MirrorheadOSC/MirrorheadOSC.py
@@ -19,48 +19,30 @@ if __name__ == "__main__":
 
     time.sleep(1)
 
-    msg = osc_message_builder.OscMessageBuilder(address = "/mdc_layer1_preset19")
+    msg = osc_message_builder.OscMessageBuilder(address = "/mdc_layer1_preset2")
     msg = msg.build()
     client.send(msg)
    
     time.sleep(1)
 
-    msg = osc_message_builder.OscMessageBuilder(address = "/mdc_layer1_preset13")
+    msg = osc_message_builder.OscMessageBuilder(address = "/mdc_layer1_preset3")
     msg = msg.build()
     client.send(msg)
 
     time.sleep(1)
 
-    msg = osc_message_builder.OscMessageBuilder(address = "/mdc_layer1_preset14")
+    msg = osc_message_builder.OscMessageBuilder(address = "/mdc_layer1_preset4")
     msg = msg.build()
     client.send(msg)
 
     time.sleep(1)
 
-    msg = osc_message_builder.OscMessageBuilder(address = "/mdc_layer1_preset15")
+    msg = osc_message_builder.OscMessageBuilder(address = "/mdc_layer1_preset5")
     msg = msg.build()
     client.send(msg)
 
     time.sleep(1)
 
-    msg = osc_message_builder.OscMessageBuilder(address = "/mdc_layer1_preset16")
-    msg = msg.build()
-    client.send(msg)
-
-    time.sleep(1)
-
-    msg = osc_message_builder.OscMessageBuilder(address = "/mdc_layer1_preset17")
-    msg = msg.build()
-    client.send(msg)
-
-    time.sleep(10)
-
-    msg = osc_message_builder.OscMessageBuilder(address = "/mdc_layer1_preset18")
-    msg = msg.build()
-    client.send(msg)
-
-    time.sleep(1)
-
-    msg = osc_message_builder.OscMessageBuilder(address = "/mdc_layer1_preset20")
+    msg = osc_message_builder.OscMessageBuilder(address = "/mdc_layer1_media4")
     msg = msg.build()
     client.send(msg)
