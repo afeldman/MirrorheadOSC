f70c56b1bb2270d559fb363469a8aa9085e69f5f
emea\feldmaa <anton.feldmann@daimler.com>
2016-01-21 09:37:14 +0100

    send two commands

--- MirrorheadOSC/MirrorheadOSC.py
+++ MirrorheadOSC/MirrorheadOSC.py
@@ -1,3 +1,25 @@
-import pythonocs
+"""
+This program sends 10 random values between 0.0 and 1.0 to the /filter address,
+waiting for 1 seconds between each value.
+"""
+import time
+
+from pythonosc import osc_message_builder
+from pythonosc import udp_client
 
 
+if __name__ == "__main__":
+    ip = "192.168.0.200"
+    port = 7475
+
+    client = udp_client.UDPClient(ip, port)
+
+    msg = osc_message_builder.OscMessageBuilder(address = "/mdc_layer1_presert1")
+    msg = msg.build()
+    client.send(msg)
+   
+    time.sleep(5)
+
+    msg = osc_message_builder.OscMessageBuilder(address = "/mdc_layer1_presert2")
+    msg = msg.build()
+    client.send(msg)
